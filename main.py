05cb124683a331da140923688c3488e9c6a7bdd6
dyspo1337 <152763773+dyspo1337@users.noreply.github.com>
2024-08-08 12:43:12 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+enterUsername = input("Username : ")
+enterPassword = input("Password : ")
+enterService = input("Service : ")
+
+
+### CREATE VAULT
+
+
+def createVault():
+    input("Vault Name : ")
